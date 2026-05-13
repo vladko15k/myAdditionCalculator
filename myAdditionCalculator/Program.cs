1e1ffea8b9deb7f80f22909b2708234be970a5cb
Lenovo <Lenovo@DESKTOP-LRUR5PJ>
2026-05-13 14:02:01 +0300

    Add project files.

---
+++ myAdditionCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter the first whole number:");
+
+int myNumber = 0;
+
+string userInput = Console.ReadLine();
+
+myNumber = int.Parse(userInput);
+
+Console.WriteLine("You entered " + userInput);
+
+Console.WriteLine("Enter the second whole number:");
+
+int myNumber2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"The sum of your numbers " +
+    $"{myNumber} and {myNumber2} is: {myNumber + myNumber2}");
+
+Console.ReadKey();
+
